46cd27c00c4dc9fb835b8515776cedaa18e04d48
Yera <ynms1@alu.ua.es>
2020-12-10 19:09:58 +0100

    Lista para entregar

--- PracticaPHP/resCrearAlbum.php
+++ PracticaPHP/resCrearAlbum.php
@@ -13,7 +13,7 @@ include('header.php');
 ?>
 
 <main>
-    <section class="col-4 margin_auto padding20">
+    <section class="formyera">
         <br><br>
         <h2 class="white text_shadow">Se ha registrado la solicitud</h2>
         <br><br>
@@ -21,10 +21,10 @@ include('header.php');
         $titulo = mysqli_real_escape_string($conexion, $_POST['titulo']);
         $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
         $usuario = mysqli_real_escape_string($conexion, $_SESSION['sesion']['IdUsuario']);
-
-        echo "<p style='color:white;'>Título: " . $titulo . "</p>";
-        echo "<p style='color:white;'>Descripcion: " . $descripcion . "</p>";
-        echo "<p style='color:white;'>ID Usuario: " . $usuario . "</p>";
+        $IdAlbum = "";
+        echo "<p style='color:black;'>Título: " . $titulo . "</p>";
+        echo "<p style='color:black;'>Descripcion: " . $descripcion . "</p>";
+        echo "<p style='color:black;'>ID Usuario: " . $usuario . "</p>";
 
 
         include("conexionBD.php");
@@ -32,7 +32,17 @@ include('header.php');
         $sql = "INSERT INTO ALBUMES(Titulo,Descripcion,Usuario) VALUES ('$titulo','$descripcion','$usuario')";
 
         if ($conexion->query($sql) === TRUE) {
-            echo "<a href='añadirfoto.php?album=$titulo'>Añade fotos a tu Album! </a>";
+            $sql = "SELECT * from ALBUMES where Usuario='$usuario'";
+            $resultados = $conexion->query($sql);
+            while ($fila = $resultados->fetch_assoc()) {
+                if( $fila['Titulo']==$titulo){
+                    $IdAlbum = $fila['IdAlbum'];
+                }
+                
+            
+            }
+            
+            echo "<a href='añadirfoto.php?album=$IdAlbum'>Añade fotos a tu Album! </a>";
         }
 
 
